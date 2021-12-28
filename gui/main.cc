8d8b35647ed2997a0dd718f60531574134c5c33d
kyler-harrison <harri828@purdue.edu>
2021-12-28 14:23:09 -0500

    window sizes

--- gui/main.cc
+++ gui/main.cc
@@ -1,10 +1,13 @@
 #include "topLayout.hh"
+#include <iostream>
 #include <QApplication> 
 
 int main( int argc, char **argv )
 {
     QApplication app(argc, argv);
 		TopLayout *window = new TopLayout();
+		window->setMinimumSize(600, 300);
+		window->setWindowState(Qt::WindowMaximized);
 		window->show();
 
     return app.exec();
