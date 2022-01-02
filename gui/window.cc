b4074ddac6e8afd9651bdbc68cc0a96bc89f65c9
kyler-harrison <harri828@purdue.edu>
2022-01-01 19:03:05 -0500

    now a main window in order to handle closeEvent(), QT and memory management is being annoying

--- gui/window.cc
+++ gui/window.cc
@@ -1,12 +1,28 @@
 #include "window.hh"
-#include "parentLayout.hh"
-#include <QWidget>
+
+#include <iostream>
 
 /*
  *  Sets ParentLayout to main layout of main window.
  */
 
-Window::Window(QWidget *parent) : QWidget(parent) {
-	ParentLayout *parentLayout = new ParentLayout(this);
-	setLayout(parentLayout);
+Window::Window(QWidget *parent) : QMainWindow(parent) {
+	//this->parentLayout = new ParentLayout(this);
+	ParentLayout *parentLayout = new ParentLayout();
+	this->parentWidget = new QWidget();
+	this->parentWidget->setLayout(parentLayout);
+	this->setCentralWidget(this->parentWidget);
+}
+
+void Window::cleanup() {
+	delete (this->parentWidget);
+	//this->parentLayout->cleanup();
+
+	// do anything else ("are you sure?" prompt, etc...)
+}
+
+void Window::closeEvent(QCloseEvent *event) {
+	std::cout << "closing...\n";
+	//event->accept();
+	this->cleanup();
 }
--- gui/window.hh
+++ gui/window.hh
@@ -1,10 +1,21 @@
+#include "parentLayout.hh"
+
+#include <QMainWindow>
 #include <QWidget>
+#include <QCloseEvent>
 
 /*
  *  Main window of application to be called show() on.
  */
 
-class Window : public QWidget {
+class Window : public QMainWindow {
+	private:
+		void closeEvent(QCloseEvent *event);
+		QWidget *parentWidget;
+		//ParentLayout *parentLayout;
+
 	public:
 		Window(QWidget *parent = nullptr);
+		void cleanup();
 };
+
