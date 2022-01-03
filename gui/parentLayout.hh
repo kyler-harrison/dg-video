0161c4511d2fdb3ceba830c545d3b33ff128bf34
kyler-harrison <harri828@purdue.edu>
2022-01-02 23:42:34 -0500

    now a Q_OBJECT, can be passed as a parent QWidget

--- gui/parentLayout.hh
+++ gui/parentLayout.hh
@@ -1,10 +1,13 @@
 #include <QHBoxLayout>
+#include <QObject>
 
 /*
  *  Upper most parent layout to be included by the main window.
  */
 
 class ParentLayout : public QHBoxLayout {
+	Q_OBJECT
+
 	public:
 		ParentLayout(QWidget *parent = nullptr);
 };
