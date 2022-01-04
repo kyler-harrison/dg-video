5ca53d52df41042aa46062813c31b714e54654a9
kyler-harrison <harri828@purdue.edu>
2022-01-04 16:44:08 -0500

    main window does not need to be heap allocated

--- gui/main.cc
+++ gui/main.cc
@@ -8,16 +8,13 @@
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-		Window *window = new Window();
-		window->showMaximized();
-		window->setWindowState(Qt::WindowMaximized);
-		window->show();
+		Window window;
+		window.showMaximized();
+		window.setWindowState(Qt::WindowMaximized);
+		window.show();
 
 		// start app main event loop
     app.exec();
-
-		// i believe this also frees all heap-allocated children of window (everything?)
-		delete window;  
 
 		return 0;
 }
