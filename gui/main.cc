f7c7c28439180e83759e18a382c6f19de04131ea
kyler-harrison <harri828@purdue.edu>
2022-01-05 14:25:04 -0500

    fixed weird formatting

--- gui/main.cc
+++ gui/main.cc
@@ -5,16 +5,15 @@
 #include <QDesktopWidget>
 #include <QFileDialog>
 
-int main(int argc, char **argv)
-{
-    QApplication app(argc, argv);
-		Window window;
-		window.showMaximized();
-		window.setWindowState(Qt::WindowMaximized);
-		window.show();
+int main(int argc, char **argv) {
+	QApplication app(argc, argv);
+	Window window;
+	window.showMaximized();
+	window.setWindowState(Qt::WindowMaximized);
+	window.show();
 
-		// start app main event loop
-    app.exec();
+	// start app main event loop
+	app.exec();
 
-		return 0;
+	return 0;
 }
