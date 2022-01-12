2632ccad2a04188750291908ebb2681656a16803
kyler-harrison <harri828@purdue.edu>
2022-01-11 23:57:51 -0500

    no longer using VideoCapture, added in several private string path params

--- cv/video.hh
+++ cv/video.hh
@@ -5,8 +5,16 @@
  */
 
 class Video {
+	private:
+		int frameIndex = 0;
+		std::string filePath = "";  // input file
+		std::string tempFramePath = "";  // where to keep temp video frames
+		std::string tempDir = "/tmp/dgvid-temp/";
+		std::string tempFramesDir = tempDir + "frames/";  
+		std::string frameName = "frame";
+		std::string ffmpegLogPath = tempDir + "logs/ffmpegOut.txt";  
+
 	public:
 		Video(std::string path = "");
-		std::string filePath;
 		cv::Mat getNextFrame();
 };
