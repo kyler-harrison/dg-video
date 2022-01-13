92089383f04d77111d57d72e6c2a9d4b008f5aa4
kyler-harrison <harri828@purdue.edu>
2022-01-12 23:33:03 -0500

    added creation/deletion of temp dir

--- cv/video.cc
+++ cv/video.cc
@@ -13,9 +13,22 @@ Video::Video(std::string path) {
 	this->filePath = path;
 	this->tempFramePath = this->tempFramesDir + this->frameName + "0.png";  // assumes all frames will be removed when done
 	this->frameIndex = 0;
-	//this->vidCap = cv::VideoCapture(this->filePath);
 
-	// TODO check for temp dirs, make if none
+	// rm temp dir if already exists, create temp dir
+	std::string rmCmd = "rm -rf " + this->tempDir;
+	std::string tmpDirCmd = "mkdir -p " + this->tempDir;
+	std::string logDirCmd = "mkdir -p " + this->logDir;
+	std::string framesDirCmd = "mkdir -p " + this->tempFramesDir;
+
+	const char *rmCmdc = rmCmd.c_str();
+	const char *tmpDirCmdc = tmpDirCmd.c_str();
+	const char *logDirCmdc = logDirCmd.c_str();
+	const char *framesDirCmdc = framesDirCmd.c_str();
+
+	system(rmCmdc);
+	system(tmpDirCmdc);
+	system(logDirCmdc);
+	system(framesDirCmdc);
 }
 
 /*
@@ -28,23 +41,36 @@ Video::Video(std::string path) {
 cv::Mat Video::getNextFrame() {
 	// TODO do some input checks
 
-	this->frameIndex++;
-
 	// TODO check if frame already read
 
 	// path to write temp frame to 
 	this->tempFramePath = this->tempFramesDir + this->frameName + std::to_string(this->frameIndex) + ".png";
 
 	// ffmpeg cmd
-	std::string frameCommand = "ffmpeg -y -i " + this->filePath + " -vf 'select=eq(n\\," + std::to_string(this->frameIndex) + ")' -vframes 1 " + this->tempFramePath + " > " + this->ffmpegLogPath + " 2>&1";
-	char *frameCommandC = const_cast<char*>(frameCommand.c_str());
+	std::string frameCommand = "ffmpeg -y -i " + this->filePath + " -vf 'select=eq(n\\," + std::to_string(this->frameIndex) + ")' -vframes 1 " + this->tempFramePath + " > " + this->logDir + this->logName + " 2>&1";
+	const char *frameCommandC = frameCommand.c_str();
 	system(frameCommandC);
 
 	// read in what ffmpeg outputted with opencv
 	cv::Mat frame = cv::imread(this->tempFramePath);
 	cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);  // qt uses BGR while cv uses RGB
 
-	// TODO track created frame indices and rm later in other fun
+	// TODO track created frame indices 
+
+	this->frameIndex++;
 
 	return frame;
 }
+
+/*
+ *  Remove the temp directory.
+ *
+ *  @return void
+ */
+
+void Video::cleanup() {
+	std::string rmCmd = "rm -rf " + this->tempDir;
+	const char *rmCmdc = rmCmd.c_str();
+	system(rmCmdc);
+}
+
--- cv/video.hh
+++ cv/video.hh
@@ -9,12 +9,14 @@ class Video {
 		int frameIndex = 0;
 		std::string filePath = "";  // input file
 		std::string tempFramePath = "";  // where to keep temp video frames
-		std::string tempDir = "/tmp/dgvid-temp/";
+		std::string tempDir = "./dgvidTemp/";
 		std::string tempFramesDir = tempDir + "frames/";  
 		std::string frameName = "frame";
-		std::string ffmpegLogPath = tempDir + "logs/ffmpegOut.txt";  
+		std::string logDir = tempDir + "logs/";  
+		std::string logName = "log.txt";  
 
 	public:
 		Video(std::string path = "");
 		cv::Mat getNextFrame();
+		void cleanup();
 };
