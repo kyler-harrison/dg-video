42993f49694ed93b8335abfb8b68f7e80e2e9277
kyler-harrison <harri828@purdue.edu>
2022-01-16 23:11:38 -0500

    changed up layout, there is now a frame / numFrames input above the prev/next frame buttons

--- gui/vidControls.cc
+++ gui/vidControls.cc
@@ -1,5 +1,7 @@
 #include "vidControls.hh"
 
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
 
 /*
@@ -8,11 +10,26 @@
  *  @param parent QWidget* parent object
  */
 
-VidControls::VidControls(QWidget *parent, VidViewer *vidViewer) : QHBoxLayout(parent) {
-	QPushButton *backBtn = new QPushButton("back");
+VidControls::VidControls(QWidget *parent, VidViewer *vidViewer) : QVBoxLayout(parent) {
+	// frame / totalFrames input and label
+	QHBoxLayout *frameCountLayout = new QHBoxLayout();
+	QLineEdit *inpFrame = new QLineEdit();  // TODO connect return press event to something in vidViewer to jump to frameIndex
+	inpFrame->setFixedWidth(80);
+	inpFrame->setText("0");
+	QLabel *numFrames = new QLabel("/ None");  // TODO connect to something in vidViewer when vid loaded and change to number of frames
+	frameCountLayout->addWidget(inpFrame);
+	frameCountLayout->addWidget(numFrames);
+	frameCountLayout->setAlignment(inpFrame, Qt::AlignRight);
+
+	// previous and next frame buttons
+	QHBoxLayout *btnLayout = new QHBoxLayout();
+	QPushButton *prevBtn = new QPushButton("prev");
 	QPushButton *nextBtn = new QPushButton("next");
-	this->addWidget(backBtn);
-	this->addWidget(nextBtn);
-	QObject::connect(backBtn, &QPushButton::released, vidViewer, &VidViewer::handleBack);  
+	btnLayout->addWidget(prevBtn);
+	btnLayout->addWidget(nextBtn);
+	QObject::connect(prevBtn, &QPushButton::released, vidViewer, &VidViewer::handlePrev);  
 	QObject::connect(nextBtn, &QPushButton::released, vidViewer, &VidViewer::handleNext);  
+
+	this->addLayout(frameCountLayout);
+	this->addLayout(btnLayout);
 }
--- gui/vidControls.hh
+++ gui/vidControls.hh
@@ -1,13 +1,13 @@
 #pragma once
 #include "vidViewer.hh"
 
-#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 /*
  *  Bottom left video controls.
  */
 
-class VidControls : public QHBoxLayout {
+class VidControls : public QVBoxLayout {
 	public:
 		VidControls(QWidget *parent = nullptr, VidViewer *vidViewer = nullptr);
 };
